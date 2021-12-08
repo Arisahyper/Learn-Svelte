6ccdc3df6227a919ce5040677f0876e9caa93f52
Aqua Minato <sweet4182691@gmail.com>
2021-12-08 15:28:51 +0900

    Edit copylight

--- src/components/Root/Footer.svelte
+++ src/components/Root/Footer.svelte
@@ -5,7 +5,7 @@
 <body>
 	<div class="bg-gray-600 h-10 leading-10">
 		<div class="text-center object-bottom">
-			<p class="text-white">arisahyper.com {year}</p>
+			<p class="text-white">(c) arisahyper.com {year}</p>
 		</div>
 	</div>
 </body>
