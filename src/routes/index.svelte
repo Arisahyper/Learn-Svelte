b1c8bbf011bb72379f32c5cfcfaebaf95797b2a8
Aqua Minato <sweet4182691@gmail.com>
2021-12-08 12:21:35 +0900

    add image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,9 @@
 
 <div class="text-base">
 	<h1 class="text-red-500 text-base">Hello Svelte</h1>
+	<p class="text-gray-700 text-base">This is a Svelte component.</p>
+	<!-- 画像を表示 -->
+	<img src="https://picsum.photos/1280/720" alt="" />
 </div>
 
 <style></style>
