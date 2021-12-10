5c225b5216745d6a1b11b02eadffc6fd46e6bb92
Aqua Minato <sweet4182691@gmail.com>
2021-12-10 12:05:02 +0900

    Change the location of the about page

---
+++ src/components/Pages/About/About.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<main class="flex flex-wrap">
+	<article class="w-full lg:w-1/2">ここはPCだと2カラムの左側、スマホだと1カラムになります</article>
+	<aside class="w-full lg:w-1/2">ここはPCだと2カラムの右側、スマホだと1カラムになります</aside>
+</main>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,8 +1,7 @@
-<svelte:head>
-	<title>About</title>
-</svelte:head>
+<script>
+	import About from '../components/Pages/About/About.svelte';
+</script>
 
-<main class="flex flex-wrap">
-	<article class="w-full lg:w-1/2">ここはPCだと2カラムの左側、スマホだと1カラムになります</article>
-	<aside class="w-full lg:w-1/2">ここはPCだと2カラムの右側、スマホだと1カラムになります</aside>
-</main>
+<body>
+	<About />
+</body>
