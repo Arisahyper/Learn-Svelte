87bd1b89f9f22cd61678c84e57868a1031a1d3eb
Aqua Minato <sweet4182691@gmail.com>
2021-12-11 15:56:36 +0900

    Add examples of store usage to storeDemo.

---
+++ src/routes/storeDemo.svelte
@@ -0,0 +1,13 @@
+<!-- Incrementer.svelte -->
+<script>
+	import { count } from '../store/stores';
+
+	function increment() {
+		$count++; // 「$」を付けると、ストアの変数にアクセスできる
+	}
+</script>
+
+<body>
+	<button on:click={increment}>+1</button>
+	<p>{$count}</p>
+</body>
