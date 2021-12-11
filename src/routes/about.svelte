5370105b35322406522fc0a71da4cf08c0ac144c
Aqua Minato <sweet4182691@gmail.com>
2021-12-11 16:34:51 +0900

    Stopped dividing page components.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,8 @@
-<script>
-	// import About from '../components/pages/About/About.svelte';
-</script>
+<svelte:head>
+	<title>About</title>
+</svelte:head>
 
-<body>
-	<!-- <About /> -->
-</body>
+<main class="flex flex-wrap">
+	<article class="w-full lg:w-1/2">ここはPCだと2カラムの左側、スマホだと1カラムになります</article>
+	<aside class="w-full lg:w-1/2">ここはPCだと2カラムの右側、スマホだと1カラムになります</aside>
+</main>
