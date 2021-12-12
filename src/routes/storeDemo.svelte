f11fe8a1191f024c422b9844efe47eed8c380493
Aqua Minato <sweet4182691@gmail.com>
2021-12-13 04:11:06 +0900

    Add increment button.

--- src/routes/storeDemo.svelte
+++ src/routes/storeDemo.svelte
@@ -14,6 +14,10 @@
 </svelte:head>
 
 <body class="flex flex-wrap">
-	<button on:click={increment}>+1</button>
-	<p>{$count}</p>
+	<div class="py-3 mx-auto text-center">
+		<button class="bg-blue-500 hover:bg-blue-700 rounded" on:click={increment}>
+			<p class="m-2">increment</p>
+		</button>
+		<p>{$count}</p>
+	</div>
 </body>
