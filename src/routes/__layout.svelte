83e5982a1718f516eb201426d405a826d37fc95c
Aqua Minato <sweet4182691@gmail.com>
2021-12-13 04:12:20 +0900

    Fix back to home at the bottom.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,14 +10,14 @@
 	<main class="flex-grow bg-gray-100">
 		<Header />
 		<slot />
-		{#if !$isHome}
-			<div class="pl-2 py-2">
-				<a
-					href="https://learn-svelte-kappa.vercel.app/"
-					class="text-sm text-blue-500 hover:text-blue-700 bg-light-100">← Back to Home</a
-				>
-			</div>
-		{/if}
 	</main>
+	{#if !$isHome}
+		<div class="pl-2 py-2 bg-gray-100">
+			<a
+				href="https://learn-svelte-kappa.vercel.app/"
+				class="text-sm text-blue-500 hover:text-blue-700 bg-light-100">← Back to Home</a
+			>
+		</div>
+	{/if}
 	<Footer />
 </body>
