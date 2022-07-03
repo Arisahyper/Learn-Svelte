76c148bb931ac8b6b2180f7277eaa6f91d922cf9
Yoshitsugu Tahara <arisahyper0000@gmail.com>
2022-07-04 06:54:24 +0900

    .

---
+++ src/routes/CssAnimation/index.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+</script>
+
+<body>
+	<div id="stripe">123456789</div>
+	<p transition:fade={{ delay: 250 }}>Hello World</p>
+</body>
+
+<style>
+	#stripe {
+		position: relative;
+		cursor: pointer;
+		width: 177px;
+		height: 160px;
+		left: 0;
+		transition: left 5s cubic-bezier(0, 0, 1, 1);
+	}
+</style>
--- src/routes/Todo/index.svelte
+++ src/routes/Todo/index.svelte
@@ -3,15 +3,13 @@
 	import { onMount } from 'svelte';
 
 	let dataList = [];
+	let taskName: string;
+	let deadLine;
 
 	axios.defaults.baseURL = 'http://localhost:8080';
 	axios.defaults.headers.post['Access-Control-Allow-Origin'];
 	axios.defaults.headers.get['Access-Control-Allow-Origin'];
-
-	// axios.get('/api/get').then(({ data }) => {
-	// 	// console.log(data);
-	// 	dataList.push(...data);
-	// });
+	axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 	async function getTask() {
 		const res = await axios.get('/api/get');
@@ -21,30 +19,81 @@
 	}
 
 	let promise: Promise<any> = getTask();
+
+	const postHandler = () => {
+		const params = new URLSearchParams();
+		params.append('task_name', taskName);
+		params.append('dead_line', deadLine);
+		axios.post('/api/register', params).then((res) => {
+			console.log(res);
+			promise = getTask(); // thenの中で呼べばOK
+		});
+	};
+
+	const deleteHandler = (id: number) => {
+		axios
+			.delete(`/api/delete/${id}`)
+			.then((res) => {
+				console.log(res);
+				promise = getTask();
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 </script>
 
 <body>
+	<button
+		on:click={() => {
+			deleteHandler(1);
+		}}>DELETE TEST</button
+	>
 	<form action="http://localhost:8080/api/register" method="post" class="text-center">
-		<h1>taskの登録</h1>
-		<input type="text" name="task_name" placeholder="task_name" class="border border-black" />
-		<input
-			type="datetime-local"
-			name="dead_line"
-			placeholder="dead_line"
-			class="border border-black"
-		/>
-		<input type="submit" value="submit" class="border border-black" />
+		<div class="m-10">
+			<h1>Taskの登録</h1>
+			<input
+				type="text"
+				name="task_name"
+				placeholder="task_name"
+				class="border border-black px-2 py-1"
+				bind:value={taskName}
+			/>
+			<input
+				type="datetime-local"
+				name="dead_line"
+				placeholder="dead_line"
+				class="border border-black px-2 py-1"
+				bind:value={deadLine}
+			/>
+			<button type="button" class="border border-black px-2 py-1" on:click={postHandler}>
+				<p>送信</p>
+			</button>
+		</div>
 	</form>
 
 	{#await promise then data}
 		{#each [...data].reverse() as d}
-			<div>
-				<p>{d.ID}</p>
-				<p>{d.TaskName}</p>
-				<p>{d.IsDone}</p>
-				<p>{d.Deadline}</p>
-				<p>{d.CreatedAt}</p>
-				<p>{d.UpdatedAt}</p>
+			<div class="mx-20 my-12 border border-black">
+				<div class="p-10">
+					<p>ID: {d.ID}</p>
+					<p>タスク: {d.TaskName}</p>
+					<p>完了: {d.IsDone}</p>
+					<p>期限: {d.Deadline}</p>
+					<p>作成: {d.CreatedAt}</p>
+					<p>更新: {d.UpdatedAt}</p>
+					<button class="border border-black mt-2 mr-2 hover:text-white hover:bg-black">
+						<p class="mx-3 my-1">編集</p>
+					</button>
+					<button
+						class="border border-black mt-2 mr-2 hover:text-white hover:bg-black"
+						on:click={() => {
+							deleteHandler(d.ID);
+						}}
+					>
+						<p class="mx-3 my-1">削除</p>
+					</button>
+				</div>
 			</div>
 		{/each}
 	{:catch error}
