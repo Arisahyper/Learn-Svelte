ed4da25e9afa58033fb8d2111b9bf2a74e5b9a32
Yoshitsugu Tahara <arisahyper0000@gmail.com>
2022-07-04 07:02:28 +0900

    .

---
+++ src/components/common/Box.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<div class="min-h-screen flex justify-center">
+	<div class="flex items-center">
+		<slot />
+	</div>
+</div>
---
+++ src/routes/com/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Box from '../../components/common/Box.svelte';
+</script>
+
+<Box>
+	<div>
+		<h1>Hello World</h1>
+		<p>This is a simple Svelte component.</p>
+	</div>
+</Box>
